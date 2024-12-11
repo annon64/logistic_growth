e10e0d6f57d66d2276f12fa4e5a3ee9da080965b
Sabrinas Yelling <scat9551@ox.ac.uk>
2024-12-11 15:23:55 +0000

    I will commit

---
+++ session2/fit_linear_model.R
@@ -0,0 +1,33 @@
+## Script to estimate the model parameters using a linear approximation
+
+# installing and loading dplyr package into the library
+
+install.packages("dplyr")
+library(dplyr)
+
+# reading in the experiment.csv file and allocating it as the object growth_data
+
+growth_data <- read.csv("experiment.csv")
+
+## Case 1. K >> N0, t is small
+
+# t is small - therefore looking at data points where t < 1400 (prior to start of exponential growth curve)
+# using N_log (log(N)) in place of N
+
+data_subset1 <- growth_data %>% filter(t<1400) %>% mutate(N_log = log(N))
+
+# creating a linear model with response variable log(N) and explanatory variable t, sing data_subset1 so t<1400
+
+model1 <- lm(N_log ~ t, data_subset1)
+summary(model1) # used to obtain the parameter estimates for the slopes and intercepts of the linear model
+
+## Case 2. N(t) = K
+
+# creating a new object, but now with high values of t such that N is at carrying capacity (k)
+# not using log(N)
+
+data_subset2 <- growth_data %>% filter(t>2500)
+
+# N is set to 1 as t is large and N is modeled as a constant (representing progression towards carrying capacity)
+model2 <- lm(N ~ 1, data_subset2)
+summary(model2)
---
+++ session2/plot_data_and_model.R
@@ -0,0 +1,33 @@
+## Script to plot data and model
+
+# reading in the experiment.csv file and allocating it as the object growth_data
+
+growth_data <- read.csv("experiment.csv")
+
+# creating a function: inputting a specific t value will output corresponding N value using specified values of K and r
+
+logistic_fun <- function(t) {
+  
+  N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
+  
+  return(N)
+  
+}
+
+# defining function parameters
+N0 <- 879 #obtained from experiment.csv data at time t=0
+  
+r <- 0.0100086 # obtained from model 1 summary output, presents the coefficient of t
+  
+K <- 6.00e+10 # obtained form model 2 summary output, presents the intercept 
+
+# creating a graph using the logistic model
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_function(fun=logistic_fun, colour="red") +
+  
+  geom_point()
+
+  #scale_y_continuous(trans='log10')
+
+
